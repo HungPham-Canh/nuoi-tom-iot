5f75ff1f67388a81d9958da1f9a6d56f7ec48930
HungPham-Canh <hung.phamhcmut@hcmut.edu.vn>
2023-04-02 23:44:25 +0700

    configure routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import './App.css'
-// import { Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 import Sidebar from './components/sidebar'
+import Home from './pages/home'
+import Dashboard from './pages/dashboard'
+import Devices from './pages/devices'
+import Settings from './pages/settings'
+import Data from './pages/data'
 
 function App() {
   return (
-    <Container fluid className="px-0 vh-100">
-      <Sidebar />
+    <Container fluid className="px-0">
+      <Row className="min-vh-100">
+        <Col lg="1">
+          <Sidebar />
+        </Col>
+        <Col>
+          <Row>
+            <h1>Header</h1>
+          </Row>
+          <Row>
+            <Col lg="9">
+              <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/dashboard" element={<Dashboard />}></Route>
+                <Route path="/devices" element={<Devices />}></Route>
+                <Route path="/settings" element={<Settings />}></Route>
+                <Route path="/data" element={<Data />}></Route>
+              </Routes>
+            </Col>
+            <Col>
+              <h1>Notifications</h1>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     </Container>
   );
 }
--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,6 +1,7 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css'
 import Nav from 'react-bootstrap/Nav'
+import { Link } from 'react-router-dom'
+// Icons
 import homeIcon from './home-icon.svg'
 import dashboardIcon from './dashboard-icon.svg'
 import devicesIcon from './devices-icon.svg'
@@ -10,26 +11,26 @@ import dataIcon from './data-icon.svg'
 function Sidebar() {
     return (
         <>
-            <Nav className="flex-column text-center justify-content-center" defaultActiveKey="/dashboard">
+            <Nav className="h-100 flex-column text-white text-center justify-content-center " defaultActiveKey="/dashboard">
                 <Nav.Item>
                     <img src={homeIcon} alt="home" />
-                    <h6 className="fw-bold"><Nav.Link href="/home">SMART HOME</Nav.Link></h6>
+                    <h6 className="fw-bold"><Link to="/">SMART HOME</Link></h6>
                 </Nav.Item>
                 <Nav.Item>
                     <img src={dashboardIcon} alt="dashboard" />
-                    <h6><Nav.Link href="/dashboard">Dashboard</Nav.Link></h6>
+                    <h6><Link to="/dashboard">Dashboard</Link></h6>
                 </Nav.Item>
                 <Nav.Item>
                     <img src={devicesIcon} alt="devices" />
-                    <h6><Nav.Link href="/devices">Devices</Nav.Link></h6>
+                    <h6><Link as="Link" to="/devices">Devices</Link></h6>
                 </Nav.Item>
                 <Nav.Item>
                     <img src={settingsIcon} alt="settings" />
-                    <h6><Nav.Link href="/settings">Settings</Nav.Link></h6>
+                    <h6><Link as="Link" to="/settings">Settings</Link></h6>
                 </Nav.Item>
                 <Nav.Item>
                     <img src={dataIcon} alt="data" />
-                    <h6><Nav.Link href="/data">Data</Nav.Link></h6>
+                    <h6><Link as="Link" to="/data">Data</Link></h6>
                 </Nav.Item>
             </Nav >
         </>
